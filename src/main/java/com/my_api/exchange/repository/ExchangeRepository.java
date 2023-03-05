b9816aa79c2e849fdbb360b044c679424f5e3f9b
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-05 17:01:45 +0300

    chore: add currency and exchange repository

---
+++ src/main/java/com/my_api/exchange/repository/ExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.my_api.exchange.repository;
+
+import com.my_api.exchange.model.Exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeRepository extends JpaRepository<Exchange, Integer> {
+}
