29b5190a47276dfd19fe1c6bc146858c7526ebb2
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-05 17:59:43 +0300

    chore: add mapper client

--- src/main/java/com/my_api/exchange/client/mapper/CurrencyMapper.java
+++ src/main/java/com/my_api/exchange/client/mapper/CurrencyMapper.java
@@ -1,2 +1,24 @@
-package com.my_api.exchange.client.mapper;public class CurrencyMapper {
+package com.my_api.exchange.client.mapper;
+
+import com.my_api.exchange.client.model.Symbols;
+import com.my_api.exchange.model.Currency;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CurrencyMapper {
+    public List<Currency> toDomain(Symbols symbols) {
+        List<Currency> currencyList = new ArrayList<>();
+        for (Map.Entry<String, String> entry : symbols.getSymbols().entrySet()) {
+            Currency currency = Currency.builder()
+                    .symbol(entry.getKey())
+                    .value(entry.getValue())
+                    .build();
+            currencyList.add(currency);
+        }
+        return currencyList;
+    }
 }
--- src/main/java/com/my_api/exchange/client/mapper/ExchangeMapper.java
+++ src/main/java/com/my_api/exchange/client/mapper/ExchangeMapper.java
@@ -1,2 +1,36 @@
-package com.my_api.exchange.client.mapper;public class ExchangeMapper {
+package com.my_api.exchange.client.mapper;
+
+import com.my_api.exchange.client.model.Latest;
+import com.my_api.exchange.model.Currency;
+import com.my_api.exchange.model.Exchange;
+import com.my_api.exchange.repository.CurrencyRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@AllArgsConstructor
+public class ExchangeMapper {
+    private CurrencyRepository currencyRepository;
+
+    public List<Exchange> toDomain(Latest latest) {
+        List<Exchange> exchangeList = new ArrayList<>();
+        LocalDate date = LocalDate.parse(latest.getDate());
+        for (Map.Entry<String, Float> entry : latest.getRates().entrySet()) {
+            Currency currency = currencyRepository.findBySymbol(
+                    entry.getKey()
+            );
+            Exchange exchange = Exchange.builder()
+                    .currency(currency)
+                    .change(entry.getValue())
+                    .date(date)
+                    .build();
+            exchangeList.add(exchange);
+        }
+        return exchangeList;
+    }
 }
