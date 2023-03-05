51676c89ed073bf260d3557993a716ba02ba32ef
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-05 17:55:08 +0300

    fix: disable auditing and update exchange entity

--- src/main/java/com/my_api/exchange/model/Exchange.java
+++ src/main/java/com/my_api/exchange/model/Exchange.java
@@ -2,8 +2,6 @@ package com.my_api.exchange.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
@@ -13,15 +11,15 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@MappedSuperclass
-@EntityListeners(value = AuditingEntityListener.class)
 public class Exchange {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     @ManyToOne
-    @Column(nullable = false)
+    @JoinColumn(name = "currency_id", nullable = false)
     private Currency currency;
+    @Column(nullable = false)
     private Float change;
-    @CreatedDate
+    @Column(nullable = false)
     private LocalDate date;
 }
