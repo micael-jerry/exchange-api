a1184b72dff4bf65a54a43f0bc908e8ed300dcde
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-06 09:40:38 +0300

    chore: save entity from provider service

--- src/main/java/com/my_api/exchange/provider/service/SymbolToCurrencyService.java
+++ src/main/java/com/my_api/exchange/provider/service/SymbolToCurrencyService.java
@@ -1,8 +1,8 @@
-package com.my_api.exchange.Service.DomainClientService;
+package com.my_api.exchange.provider.service;
 
 import com.my_api.exchange.Service.CurrencyService;
-import com.my_api.exchange.client.mapper.CurrencyMapper;
 import com.my_api.exchange.model.Currency;
+import com.my_api.exchange.provider.mapper.CurrencyMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +10,12 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class CurrencySymbolService {
-    private com.my_api.exchange.client.service.Service clientService;
+public class SymbolToCurrencyService {
+    private ProviderService clientService;
     private CurrencyService currencyService;
     private CurrencyMapper currencyMapper;
 
-    public List<Currency> saveCurrencyFromClient() {
+    public List<Currency> saveCurrencyFromProvider() {
         List<Currency> currencyList = currencyMapper
                 .toDomain(clientService.getSymbols());
         return currencyService.saveAll(currencyList);
