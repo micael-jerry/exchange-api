5c631503bd45d44859d367921efce8d485896b75
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-10 19:31:27 +0300

    chore: add listener for save symbol at start

--- src/main/java/com/my_api/exchange/configuration/Listener.java
+++ src/main/java/com/my_api/exchange/configuration/Listener.java
@@ -1,2 +1,18 @@
-package com.my_api.exchange.configuration;public class Listener {
+package com.my_api.exchange.configuration;
+
+import com.my_api.exchange.provider.service.SymbolToCurrencyService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Configuration
+@AllArgsConstructor
+public class Listener {
+    private final SymbolToCurrencyService symbolToCurrencyService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void saveSymbolsAfterRunApp() {
+        symbolToCurrencyService.saveCurrencyFromProvider();
+    }
 }
